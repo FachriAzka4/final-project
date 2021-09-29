b000a0445926e6b8dbad9f453c612d90ec77b9c5
FachriAzka4 <90831982+FachriAzka4@users.noreply.github.com>
2021-09-30 01:31:56 +0700

    Update Nasabah.java

--- src/akun/Nasabah.java
+++ src/akun/Nasabah.java
@@ -16,20 +16,20 @@ public abstract class Nasabah {
     protected StringProperty nama, alamat;
     protected ArrayList<Rekening> rekening;
     
-    public StringProperty getNama() {
-        return nama;
+    public String getNama() {
+        return nama.get();
     }
 
-    public void setNama(StringProperty nama) {
-        this.nama = nama;
+    public void setNama(String nama) {
+        this.nama.set(nama);
     }
 
-    public StringProperty getAlamat() {
-        return alamat;
+    public String getAlamat() {
+        return alamat.get();
     }
 
-    public void setAlamat(StringProperty alamat) {
-        this.alamat = alamat;
+    public void setAlamat(String alamat) {
+        this.alamat.set(alamat);
     }
 
     public ArrayList<Rekening> getRekening(){
